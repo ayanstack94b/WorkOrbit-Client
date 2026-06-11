f623002293edec99b411542ebfe3cdd8696c513a
unknown <ayondevs.94b@gmail.com>
2026-06-11 23:09:42 +0530

    added href to dashboard sidebar options

---
+++ src/app/dashboard/recruiter/company/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecruiterCompany = () => {
+    return (
+        <div>
+            DashboardCompanyPage
+        </div>
+    );
+};
+
+export default RecruiterCompany;
--- src/components/dashboard/DashboardSidebar.jsx
+++ src/components/dashboard/DashboardSidebar.jsx
@@ -1,29 +1,32 @@
 
 
-import { Bars, Bell, Envelope, Gear, House, Magnifier, Person } from "@gravity-ui/icons";
+import { Bars, Bell, Envelope, Gear, House, Magnifier, Person, Briefcase } from "@gravity-ui/icons";
 import { Button, Drawer } from "@heroui/react";
+import Link from "next/link";
 
 export default function dashboardSidebar() {
     const navItems = [
-        { icon: House, label: "Home" },
-        { icon: Magnifier, label: "Search" },
-        { icon: Bell, label: "Notifications" },
-        { icon: Envelope, label: "Messages" },
-        { icon: Person, label: "Profile" },
-        { icon: Gear, label: "Settings" },
+        { icon: House, label: "Home", href: "/dashboard/recruiter" },
+        { icon: Magnifier, label: "Jobs", href: "/dashboard/recruiter/jobs" },
+        { icon: Bell, label: "Create a Job", href: "/dashboard/recruiter/jobs/new" },
+        { icon: Briefcase, label: "Company Profile", href: "/dashboard/recruiter/company" },
+        { icon: Envelope, label: "Messages", href: "/dashboard/recruiter" },
+        { icon: Person, label: "Company Profile", href: "/dashboard/recruiter/company-profile" },
+        { icon: Gear, label: "Settings", href: "/dashboard/recruiter/settings" },
     ];
 
     const navContent =
         <nav className="flex flex-col gap-1">
             {navItems.map((item) => (
-                <button
+                <Link
                     key={item.label}
                     className="flex items-center gap-3 rounded-xl px-3 py-2.5 text-sm text-foreground transition-colors hover:bg-default"
                     type="button"
+                    href={item.href}
                 >
                     <item.icon className="size-5 text-muted" />
                     {item.label}
-                </button>
+                </Link>
             ))}
         </nav>
     return (
